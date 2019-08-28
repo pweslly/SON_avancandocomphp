1262468f9d749d864abe2c105c646b6a6990e6b6
Pw <pablo_weslly@hotmail.com>
2019-08-27 22:22:23 -0300

    fix: modficação adicionada final

--- app/index.php
+++ app/index.php
@@ -2,4 +2,8 @@
 
 // empty() -> verifica sem "", 0, false , array()
 // trim() -> ele tira espaço no inicio ou fim, e tb pode tirar uma letra
-()strip_tags()  --> ele serve para tirar codigos escrito em htm
+//()strip_tags()  --> ele serve para tirar codigos escrito em htm
+
+//var_dump($_SERVER);
+
+var_dump($_RESQUET);
